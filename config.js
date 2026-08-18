8892598f5188b00365a0e55720e51274a60de662
Flydown98 <qazw1385@gmail.com>
2026-08-18 13:37:26 +0900

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 window.NYJ20_CONFIG = {
   // Apps Script 배포 후 받은 /exec 주소를 아래 따옴표 안에 붙여넣으세요.
-  appsScriptUrl: 'PASTE_YOUR_APPS_SCRIPT_EXEC_URL_HERE',
+  appsScriptUrl: 'https://script.google.com/macros/s/AKfycbwAXRsB9-GfkQ2nTaxy1s0tTsecyu33SL8QjOa7KlykGAJI5xaNyY8i3SCIScp-FV3_/exec',
   requestTimeoutMs: 25000,
   defaultAutoRefreshSeconds: 15
 };
