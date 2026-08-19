4bda8b4381795f138d41284e1302a027c7ce30f4
Flydown98 <qazw1385@gmail.com>
2026-08-19 16:39:43 +0900

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 window.NYJ20_CONFIG = {
   // 현재 GitHub Pages에서 사용 중인 Apps Script /exec 주소입니다.
-  appsScriptUrl: 'https://script.google.com/macros/s/AKfycbzmyZETdN0QyKLdEJA0vw0DiVkDrANpBg0b51HN0Z26NP-QcxnBNsAAcGsS_myPoSov/exec',
+  appsScriptUrl: 'https://script.google.com/macros/s/AKfycbyqDm0WPM43BTwqwxvdcnKBWoFroG4iYfcxWJ4XGOHDzzI-GAXJfnawUqvs0glWD6l0/exec',
   requestTimeoutMs: 25000,
   defaultAutoRefreshSeconds: 15
 };
